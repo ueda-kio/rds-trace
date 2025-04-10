7afdeba867f77c6a0d131f890b5d0e18e0021903
上田貴央 <gibson511511@gmail.com>
2025-04-10 22:33:23 +0900

    trace

--- app/.runtime_env/env.base.js
+++ app/.runtime_env/env.base.js
@@ -2,8 +2,12 @@
  * 環境変数
  */
 module.exports = {
-	serverRuntimeConfig: {
-		BACKEND_URL: process.env.BACKEND_URL,
-	},
-	publicRuntimeConfig: {},
+  serverRuntimeConfig: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    REDIS_HOST: process.env.REDIS_HOST,
+    REDIS_PORT: process.env.REDIS_PORT,
+  },
+  publicRuntimeConfig: {
+    APP_RUNTIME_ENV: process.env.APP_RUNTIME_ENV,
+  },
 };
--- app/.runtime_env/schema.ts
+++ app/.runtime_env/schema.ts
@@ -1,12 +1,15 @@
 import { z } from "zod";
 
 export const serverRuntimeConfigSchema = z.object({
-	// INTERNAL BE
-	BACKEND_URL: z.string(),
+  // INTERNAL BE
+  BACKEND_URL: z.string(),
+  REDIS_HOST: z.string().optional(),
+  REDIS_PORT: z.string().optional(),
 });
 
 export const publicRuntimeConfigSchema = z.object({
-	API_MOCKING: z.enum(["external", "rid-only", "dev", "enabled"]).optional(),
+  APP_RUNTIME_ENV: z.string(),
+  API_MOCKING: z.enum(['external', 'rid-only', 'dev', 'enabled']).optional(),
 });
 
 export type ServerRuntimeConfig = z.infer<typeof serverRuntimeConfigSchema>;
